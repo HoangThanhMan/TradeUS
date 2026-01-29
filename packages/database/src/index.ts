2ef474869d42810c8243a3c0f408fe9567f97c53
Longnd2525 <l66l65ll@gmail.com>
2026-01-29 12:01:35 +0700

    small re-adjustment

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -4,7 +4,7 @@ export * from './mongoose/abstract.repository';
 export * from './mongoose/schemas/user.schema';
 export * from './mongoose/schemas/qr-config.schema';
 export * from './mongoose/schemas/vip-request.schema';
-//export * from './mongoose/schemas/payment.schema';
+export * from './mongoose/schemas/payment.schema';
 // export * from './mongoose/schemas/subscription.schema';
 // export * from './mongoose/schemas/sentiment.schema';
 
